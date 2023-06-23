857e7e1d8f6945e88dc96c1f723c21394a7dea1b
kalyani <115893046+Bogalakalyani@users.noreply.github.com>
2023-06-23 15:53:43 +0530

    Add files via upload

---
+++ binary_tree.py
@@ -0,0 +1,84 @@
+class BinaryTree:
+    def __init__(self,data):
+        self.data = data
+        self.lchild = None
+        self.rchild = None
+
+    def insertion(self,key):
+        if self.data == None:
+            self.data = key
+            return  
+        if self.data > key:
+            if self.lchild:
+                self.lchild.insertion(key)
+            else:
+                self.lchild = BinaryTree(key)
+        else:
+            if self.rchild:
+                self.rchild.insertion(key)
+            else:
+                self.rchild = BinaryTree(key)
+    
+    def search(self,key):
+        if self.data == key:
+            print("node found")
+            return
+        if self.data > key:
+            if self.lchild:
+                self.lchild.search(key)
+            else:
+                print("node not found")
+        else:
+            if self.rchild:
+                self.rchild.search(key)
+            else:
+                print("node not found")
+            
+    def preorder(self):
+        print(self.data)
+        if self.lchild:
+            self.lchild.preorder()
+        if self.rchild:
+            self.rchild.preorder()
+
+    def inorder(self):
+        if self.lchild:
+            self.lchild.preorder()
+        print(self.data)
+        if self.rchild:
+            self.rchild.preorder()
+    def postorder(self):
+        if self.lchild:
+            self.lchild.postorder()
+        if self.rchild:
+            self.rchild.postorder()
+        print(self.data)
+    def bfs(self):
+        queue = []
+        queue.append(self.data)
+        if self.data != None:
+           queue.append(self.data)
+
+        print(queue[0])
+
+
+node = BinaryTree(None)
+list = [60,20,30,40,50,70,80]
+for i in list:
+    node.insertion(i)
+print("preorder")
+node.preorder()
+print("inorder")
+node.inorder()
+print("postorder")
+node.postorder()
+node.search(60)
+node.search(100)
+print(node.data)
+print(node.lchild.data)
+print(node.rchild.data)
+print(node.lchild.lchild.data)
+print(node.lchild.rchild.data)
+
+
+#breadth first search
